73a5d9db7f2149e7bcb03b0810ce26196b1cfc32
Alwan AR <alwanar210801@gmail.com>
2024-10-31 10:16:38 +0800

    feat : setup project resource

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,102 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        app: {
+          brand: {
+            500: "#44D5CD",
+            600: "#25A59E",
+          },
+          primary: "#25A59E",
+          neutral: {
+            500: "#8C8C8C",
+            600: "#737373",
+            700: "595959",
+            900: "#262626",
+          },
+          white: {
+            100: "#ffffff",
+            300: "rgba(255, 255, 255, 0.8)",
+          },
+        },
+      },
+      fontSize: {
+        "display-desktop-1": [
+          "170px",
+          {
+            fontWeight: "400",
+            lineHeight: "170px",
+          },
+        ],
+        "display-mobile-1": [
+          "52px",
+          {
+            fontWeight: "400",
+          },
+        ],
+        "header-desktop-1": [
+          "72px",
+          {
+            fontWeight: "400",
+            lineHeight: "84px",
+          },
+        ],
+        "header-desktop-2": [
+          "72px",
+          {
+            fontWeight: "400",
+            lineHeight: "84px",
+          },
+        ],
+        "header-desktop-3": [
+          "40px",
+          {
+            fontWeight: "700",
+            lineHeight: "62px",
+          },
+        ],
+        "header-mobile-1": [
+          "40px",
+          {
+            fontWeight: "400",
+            lineHeight: "52px",
+          },
+        ],
+        "header-mobile-2": [
+          "28px",
+          {
+            fontWeight: "400",
+            lineHeight: "40px",
+          },
+        ],
+        "header-mobile-3": [
+          "20px",
+          {
+            fontWeight: "700",
+            lineHeight: "28px",
+          },
+        ],
+        "body-desktop-18": [
+          "18px",
+          {
+            lineHeight: "32px",
+          },
+        ],
+        "body-tablet-16": [
+          "16px",
+          {
+            lineHeight: "24px",
+          },
+        ],
+        "body-mobile-13": [
+          "13px",
+          {
+            lineHeight: "22px",
+          },
+        ],
+      },
+      fontFamily: {
+        urbanist: ["var(--font-urbanist)"],
+        gilda: ["var(--gilda-display)"],
       },
     },
   },
